9acd679950552698502206a51676b53b4f488783
Hui <xuhuizhu369@gmail.com>
2021-04-21 17:58:54 -0700

    track products left of and right of array to generate productArray in O(n) O(n)

--- src/productArray.ts
+++ src/productArray.ts
@@ -16,3 +16,34 @@ export function bf_arrayOfProducts(array: number[]) {
     
 return prodArr;
 }
+
+export function arrayOfProducts(array: number[]) {
+// Write your code here.
+    let productsLeft: number[] = array.map(_ => 1);
+    let productsRight: number[] = array.map(_ => 1);
+    
+    let runningProd = 1;
+    for (let i = 0; i < array.length; i++) {
+        productsLeft[i] = runningProd;
+        runningProd *= array[i];
+    }
+    
+    runningProd = 1;
+    for (let i = array.length-1; i > -1; i--) {
+        productsRight[i] = runningProd;
+        runningProd *= array[i];
+    }
+    
+    for (let i = 0; i < array.length; i++) {
+        array[i] = productsLeft[i] * productsRight[i];
+    }
+    
+return array;
+}
+/*
+[5, 1, 4, 2]
+product <-
+[1, 5, 5, 20]
+product ->
+[8, 8, 2, 1]
+*/
