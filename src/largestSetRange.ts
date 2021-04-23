31aba18dbb9230b2733c82f1690f344ae3a4177e
Hui <xuhuizhu369@gmail.com>
2021-04-23 15:16:19 -0700

    determines largest setRange by hashing into a set and iterating min/max

--- src/largestSetRange.ts
+++ src/largestSetRange.ts
@@ -45,3 +45,44 @@ export function bf_largestRange(array: number[]): [number, number] {
     
 return range;
 }
+
+export function largestRange(array: number[]): [number, number] {
+// Write your code here.
+    const numSet = new Set(array);
+    
+    const max = Math.max(...array);
+    const min = Math.min(...array);
+    
+    let range: [number, number] = [min, min];
+    let curR: [number, number] = [NaN, NaN];
+    
+    for (let i = min; i < max+1; i++) {
+        
+        if ( numSet.has(i) && !isNaN(curR[0]) ) {
+            // in middle of range
+            curR[1] = i;
+        } else 
+        if ( numSet.has(i) && isNaN(curR[0]) ) {
+            // start of a new range
+            curR[0] = i;
+            curR[1] = i;
+        } else 
+        if ( !numSet.has(i) && !isNaN(curR[0]) ) {
+            // end range
+            if ( curR[1] - curR[0] > range[1] - range[0] ) {
+                [range[0], range[1]] = [curR[0], curR[1]];
+            }
+            
+            [curR[0], curR[1]] = [NaN, NaN];
+        } else 
+        if ( !numSet.has(i) && isNaN(curR[0]) ) {
+            // do nothing
+        }
+    }
+    
+    if ( curR[1] - curR[0] > range[1] - range[0] ) {
+        [range[0], range[1]] = [curR[0], curR[1]];
+    }
+    
+return range;
+}
