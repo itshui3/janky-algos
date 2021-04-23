92aa00d83ebff3849474f27493568da94d22d8c9
Hui <xuhuizhu369@gmail.com>
2021-04-23 03:30:46 -0700

    mutates array into negatives to account for referencd indices

--- src/firstDuplicateValue.ts
+++ src/firstDuplicateValue.ts
@@ -1,4 +1,4 @@
-export function firstDuplicateValue(array: number[]) {
+export function bf_firstDuplicateValue(array: number[]) {
 // Write your code here.
     
     let numHash: { [key: string]: number } = {};
@@ -11,4 +11,15 @@ export function firstDuplicateValue(array: number[]) {
     }
     
 return -1;
+}
+
+export function firstDuplicateValue(array: number[]) {
+// Write your code here.
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[ Math.abs(array[i])-1 ] < 0) return Math.abs(array[i]);
+        array[ Math.abs(array[i])-1 ] *= -1;
+    }
+    
+return -1
 }
