bf3d68d7399abefd2585023fa764a6b42f57b106
Hui <xuhuizhu369@gmail.com>
2021-04-24 20:30:07 -0700

    writes fail-cases more separately

--- src/validateMountainArray.ts
+++ src/validateMountainArray.ts
@@ -28,3 +28,34 @@ export function validMountainArray(arr: number[]): boolean {
     
     return true;
 };
+
+export function cleaner_validMountainArray(arr: number[]): boolean {
+    // fail-case: mountain not possible
+    if (arr.length < 3) return false;
+    // fail-case: flatness occurs
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === arr[i-1]) return false;
+    }
+    // fail-case: incline does not occur
+    if (arr[1] < arr[0]) return false;
+
+    // handles incline
+    let inclining = true;
+    let i = 2;
+    while (i < arr.length && inclining) {
+
+        if (arr[i] < arr[i-1]) inclining = false;
+        i++;
+        
+    }
+    
+    // fail-case: never declines
+    if (i === arr.length && inclining) return false;
+    
+    // handles decline
+    for (; i < arr.length; i++) {
+        if (arr[i] > arr[i-1]) return false;
+    }
+    
+    return true;
+};
