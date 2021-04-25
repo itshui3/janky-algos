3c97d61ca31dc96688587e50858bd306ccedf640
Hui <xuhuizhu369@gmail.com>
2021-04-24 21:34:56 -0700

    adds exposition notes in comments

--- src/longestPeak.ts
+++ src/longestPeak.ts
@@ -14,10 +14,15 @@ export function messy_longestPeak(array: number[]) {
       for (let i = 1; i < array.length; i++) {
           let curIncline = array[i] - array[i-1];
   
+          // case: currently decreasing
+          // if in peak, peak should increment
           if (curIncline < 0) {
               if (curLength) curLength++;
           } else 
   
+          // case: currently flat
+          // curLength always negates to 0
+          // if peak ends, check longest
           if (!curIncline) {
               if (prevIncline < 0 && curLength) {
                   if (curLength > longest) longest = curLength;
@@ -26,6 +31,10 @@ export function messy_longestPeak(array: number[]) {
               curLength = 0;
           } else
   
+          // case: currently increasing
+          // if peak starting, account for 2
+          // if in peak, increment peak
+          // if ending a peak, check longest and reset curLength
           if (curIncline > 0) {
               if (prevIncline < 0 && curLength > longest) {
                   longest = curLength;
@@ -36,9 +45,11 @@ export function messy_longestPeak(array: number[]) {
               else {curLength = 2;}
           }
   
+          // assigns current to previous incline
           prevIncline = curIncline;
       }
       
+      // if last stretch consists of peak, check longest
       if (curLength > longest && prevIncline < 0) longest = curLength;
       
     return longest;
