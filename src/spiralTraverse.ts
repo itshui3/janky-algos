49d2fa08e17efbc73855fc2c5d3017bcf15517ae
Hui <xuhuizhu369@gmail.com>
2021-04-26 02:01:53 -0700

    cleaned up spiral traverse, simplifies direction validation

--- src/spiralTraverse.ts
+++ src/spiralTraverse.ts
@@ -1,61 +1,54 @@
 export function spiralTraverse(array: number[][]) {
-// Write your code here.
-    let expectedLength = array.length * array[0].length;
-    
-    let dir = {
-        n: [-1, 0],
-        e: [0, 1],
-        s: [1, 0],
-        w: [0, -1]
-    };
-    
-    let i = [0, 0];
-    let curDir = dir.n;
+  // Write your code here.
+	let expectedLength = array.length * array[0].length;
+	
+	let dir = {
+		n: [-1, 0],
+		e: [0, 1],
+		s: [1, 0],
+		w: [0, -1]
+	};
+	
+	let i: [number, number] = [0, 0];
+	
+	let dirInterface = [dir.e, dir.s, dir.w, dir.n];
+	let cd = 0;
+	let curDir = dirInterface[cd];
+	
+	let tracker: Set<string> = new Set();
+	let log: number[] = [];
 
-    let tracker: Set<number> = new Set();
-    let log = [];
-    
-    while (true) {
-        // [1] log position
-        log.push(array[ i[0] ] [ i[1] ]);
-        
-        if (log.length === expectedLength) break;
-        // [2] determine direction
-        let changeDir = true;
-        while (changeDir) {
-            
-            let nRow = curDir[0]+i[0];
-            let nCol = curDir[1]+i[1];
+	for (let x = 0; x < expectedLength; x++) {
+		// [0] move i
+		if (x > 0) [i[0], i[1]] = [curDir[0]+i[0], curDir[1]+i[1]];
+		
+		// [1] log position
+		log.push(array[i[0]][i[1]]);
+		tracker.add(`${i[0]}.${i[1]}`);
+		
+		if (log.length === expectedLength) break;
+		// [2] determine next direction
+		let nRow = curDir[0]+i[0];
+		let nCol = curDir[1]+i[1];
 
-            switch(curDir) {
-                case dir.n:
-                    if(i[0] === 0 || tracker.has( array[nRow][nCol] )) curDir = dir.e;
-                    else changeDir = false;
-                    break;
+		// validate direction
+		while (
+			// [a] wall
+			nRow < 0 || 
+			nCol < 0 || 
+			nRow >= array.length || 
+			nCol >= array[0].length ||
+			// [b] visited
+			tracker.has(`${nRow}.${nCol}`)
+		) {
+			cd++;
+			curDir = dirInterface[cd % 4];
 
-                case dir.e:
-                    if(i[0] === 0 || tracker.has( array[nRow][nCol] )) curDir = dir.s;
-                    else changeDir = false;
-                    break;
+			nRow = curDir[0]+i[0];
+			nCol = curDir[1]+i[1];
+		}
 
-                case dir.s:
-                    if(i[0] === 0 || tracker.has( array[nRow][nCol] )) curDir = dir.w;
-                    else changeDir = false;
-                    break;
-
-                case dir.w:
-                    if(i[0] === 0 || tracker.has( array[nRow][nCol] )) curDir = dir.n;
-                    else changeDir = false;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        
-        // [3] move i
-        [i[0], i[1]] = [curDir[0]+i[0], curDir[1]+i[1]];
-    }
-    
-return log.length ? log : [-1];
+	}
+	
+  return log.length ? log : [-1];
 }
