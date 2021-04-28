6d8752d4dc55366a7b9c8677a462c5eb4835d610
Hui <xuhuizhu369@gmail.com>
2021-04-27 20:53:27 -0700

    adds an optimal left to right, right to left loop solution for apartmentHunting

--- src/apartmentHunting.ts
+++ src/apartmentHunting.ts
@@ -6,7 +6,7 @@ interface distanceObj {
     [key: string]: number;
 }
 
-export function apartmentHunting(blocks: Block[], reqs: string[]) {
+export function doubleLoop_apartmentHunting(blocks: Block[], reqs: string[]) {
     /*
     for each block
     construct an object with each req as key
@@ -53,4 +53,57 @@ export function apartmentHunting(blocks: Block[], reqs: string[]) {
     });
 
 return compiledDistances.indexOf(Math.min(...compiledDistances));
+}
+
+export function leftRight_apartmentHunting(blocks: Block[], reqs: string[]) {
+	let distances = blocks.map(_ => {
+		let dObj: distanceObj = {};
+		reqs.forEach(req => dObj[req] = NaN);
+		return dObj;
+	});
+	
+	let leftD: distanceObj = {};
+	reqs.forEach(req => leftD[req] = NaN);
+	
+	for (let l = 0; l < blocks.length; l++) {
+		// [0] if req NaN, check for it (put as 0 if exists)
+		// [1] if req exist, check for it (put as +1 if not exist)
+		reqs.forEach(req => {
+			if (!isNaN(leftD[req])) leftD[req] += 1;
+			
+			if (blocks[l][req]) {
+				leftD[req] = 0;
+				distances[l][req] = leftD[req];
+			} else {
+				distances[l][req] = leftD[req];
+			}
+			
+		});
+	}
+	
+	let rightD: distanceObj = {};
+	reqs.forEach(req => rightD[req] = NaN);
+	
+	for (let r = blocks.length-1; r > -1; r--) {
+		
+		reqs.forEach(req => {
+			if (!isNaN(rightD[req])) rightD[req] += 1;
+			
+			if (blocks[r][req]) {
+				rightD[req] = 0;
+				distances[r][req] = rightD[req];
+			} else {
+				// if either NaN, or distances[r][req] < rightD[req] : don't assign
+				if (isNaN(distances[r][req]) || rightD[req] < distances[r][req]) distances[r][req] = rightD[req];
+			}
+			
+		})
+	}
+	
+	const compiledDistances = distances.map(d => {
+		return Object.values(d).reduce((a, b) => a > b ? a : b, 0);
+	});
+
+  return compiledDistances.indexOf(Math.min(...compiledDistances));
+
 }
