7eecfded81d92401a81c34af81583a049d289449
Hui <xuhuizhu369@gmail.com>
2021-05-29 01:31:57 -0700

    re-composed interweavingStrings

--- src/interweavingString.ts
+++ src/interweavingString.ts
@@ -1,51 +1,49 @@
 export function interweavingStrings(one: string, two: string, three: string) {
 // Write your code here.
+    if ((one+two).length !== three.length) return false;
     
-    return searchInterweave(one, two, three, 0, 0, 0, '');
+    return re_weaveStrings(one, two, three, 0, 0, 0, '');
+
 }
 
-function searchInterweave(one: string, two: string, three: string, o: number, t: number, th: number, runningString: string) {
-    
-    
-    let oC = o;
-    let tC = t;
-    let thC = th;
-    
-    let curString = runningString;
-    
-    for (; thC < three.length; thC++) {
-        if (oC === one.length) {
-            if (curString+two.slice(tC, two.length) === three) return true;
-            else return false;
-        }
-        
-        if (tC === two.length) {
-            if (curString+one.slice(oC, one.length) === three) return true;
-            else return false;
-        }
-// - [] when there are two possibilities, path both recursively
-        if (three[thC] === one[oC] && three[thC] === two[tC]) {
-            if (searchInterweave(one, two, three, oC+1, tC, thC+1, curString+one[o])) return true;
-            if (searchInterweave(one, two, three, oC, tC+1, thC+1, curString+two[t])) return true;
-        } else 
-        if (three[thC] === one[oC]) {
-            curString += one[oC];
-            oC++;
-        } else
-        if (three[thC] === two[tC]) {
-            curString += two[tC];
-            tC++;
+function re_weaveStrings(
+        one: string,
+        two: string,
+        three: string,
+        o: number,
+        t: number,
+        th: number,
+        curString: string
+) {
+// base cases: 
+            // [0] - string has naturally reached it's end
+            if (th >= three.length) {
+                if (curString === three) return true;
+                else return false;
+            }
+            // [1] - first string used up
+            if (o >= one.length) {
+                if (curString + two.slice(t, two.length) === three) return true;
+                else return false;
+            }
+            // [2] - second string used up
+            if (t >= two.length) {
+                if (curString + one.slice(o, one.length) === three) return true;
+                else return false;
+            }
+            // [3] - match not found
+            if (one[o] !== three[th] && two[t] !== three[th]) {
+                return false;
+            }
+            // [4] - weave cases
+            // [a] - the 'end' character from string one matches end character in string three
+            if (one[o] === three[th]) {
+                if (re_weaveStrings(one, two, three, o+1, t, th+1, curString+one[o])) return true;
+            }
+            // [b] - the 'end' character from string two matches end character in string three
+            if (two[t] === three[th]) {
+                if (re_weaveStrings(one, two, three, o, t+1, th+1, curString+two[t])) return true;
+            }
             
-        } else return false;
-        
-    }
-    
-    if (curString === three) return true;
-    else return false;
+            return false;
 }
-
-/*
-at every index, I can choose the next character from either one or two
-I can path both by recursively invoking a function that takes in args
-indicating the previously taken path so far up to that fn call
-*/
